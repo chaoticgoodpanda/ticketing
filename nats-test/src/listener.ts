42b4769d9720274aa24cc754f3d69ad6d140622a
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-08-06 23:16:00 +0200

    Set up NATS publisher

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,2 @@
+import nats from 'node-nats-streaming';
+
---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,22 @@
+import nats from 'node-nats-streaming';
+
+// naming convention for "client" in NATS is stan (NATS backwards)
+const stan = nats.connect('ticketing', 'abc', {
+    url: 'http://localhost:4222'
+});
+
+stan.on('connect', () => {
+    console.log('Publisher connected to NATS.');
+    
+    // one hitch with NATS is that we cannot share an Object (like the below) directly to NATS
+    // need to convert it to JSON first
+    const data = JSON.stringify({
+        id: '123',
+        title: 'concert',
+        price: 20
+    });
+    
+    stan.publish('ticket:created', data, () => {
+        console.log('Event published');
+    })
+})
