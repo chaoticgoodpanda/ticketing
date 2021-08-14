de37988a5c6ca5f0505173f6653139d8000630ca
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-08-15 00:45:06 +0200

    npm updates, event handling

---
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -0,0 +1,19 @@
+
+const setup = async () => {
+
+
+    // call the onMessage function with the data object + message object
+
+    // write assertions to make sure a ticket was created
+};
+
+it('creates and saves a ticket', async () => {
+
+});
+
+it('ack the message', async () => {
+
+    // call the onMessage function with the data object + message object
+
+    // write assertions to make ack function is called
+});
