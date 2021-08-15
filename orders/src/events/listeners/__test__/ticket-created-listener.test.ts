fafaf91604de09ef70a70e34fe7a68df61621e20
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-08-15 09:59:04 +0200

    Dropbox directory syncing changes

--- orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -1,8 +1,20 @@
+import { TicketCreatedEvent} from "@mikeytickets/common";
+import {TicketCreatedListener} from "../ticket-created-listener";
+import {natsWrapper} from "../../../nats-wrapper";
+import mongoose from "mongoose";
 
 const setup = async () => {
-
+    // create an instance of the listener
+    const listener = new TicketCreatedListener(natsWrapper.client);
 
     // call the onMessage function with the data object + message object
+    const data: TicketCreatedEvent['data'] = {
+        version: 0,
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: 'concert',
+        price: 10,
+        userId: new mongoose.Types.ObjectId().toHexString()
+    };
 
     // write assertions to make sure a ticket was created
 };
--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,7 +4,6 @@ import {app} from "./app";
 import {natsWrapper} from "./nats-wrapper";
 import { TicketCreatedListener} from "./events/listeners/ticket-created-listener";
 import { TicketUpdatedListener} from "./events/listeners/ticket-updated-listener";
-import {TicketUpdatedPublisher} from "../../tickets/src/events/publishers/ticket-updated-publisher";
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
