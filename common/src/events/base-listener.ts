8dc1cd00f9fa7cfa66032f594228a42d5544537e
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-08-16 21:50:59 +0200

    updated common, set up listener that publishes its own events v2

--- common/src/events/base-listener.ts
+++ common/src/events/base-listener.ts
@@ -14,7 +14,8 @@ export abstract class Listener<T extends Event> {
     abstract subject: T['subject'];
     abstract queueGroupName: string;
     abstract onMessage(data: T['data'], msg: Message): void;
-    private client: Stan;
+    // change from private to protected for use by different NATS listeners
+    protected client: Stan;
     protected ackWait = 5 * 1000;
 
     constructor(client: Stan) {
--- tickets/src/events/publishers/ticket-updated-publisher.ts
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -1,7 +1,12 @@
 import {Publisher, Subjects, TicketUpdatedEvent} from "@mikeytickets/common";
+import { natsWrapper } from "../../nats-wrapper";
 
 
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
     readonly subject = Subjects.TicketUpdated;
     
 }
+
+new TicketUpdatedPublisher(natsWrapper.client).publish({
+
+});
