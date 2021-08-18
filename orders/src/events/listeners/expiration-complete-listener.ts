1159906de531f3de8f33cf137b5c7612b5e05373
chaoticgoodpanda <michael.lwin@koekoetech.com>
2021-08-18 09:30:29 +0200

    Set up expiration-complete-listener.test.ts and expiration-complete-listener.ts

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -1,13 +1,39 @@
-import {ExpirationCompleteEvent, Listener, Subjects} from "@mikeytickets/common";
+import {ExpirationCompleteEvent, Listener, OrderStatus, Subjects} from "@mikeytickets/common";
 import {Message} from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import {Order} from "../../models/order";
+import {OrderCanceledPublisher} from "../publishers/order-canceled-publisher";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
     queueGroupName = queueGroupName;
     readonly subject = Subjects.ExpirationComplete;
 
-    // incoming data attemping to say that an order should be canceled
+    // incoming data attempting to say that an order should be canceled
     async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const order = await Order.findById(data.orderId).populate('ticket');
 
+        // check if order is defined
+        if (!order) {
+            throw new Error('Order not found');
+        }
+
+        order.set({
+            // we don't need to clear out ticket as ticket: null due to the isReserved property we created in ticket.ts
+            status: OrderStatus.Canceled,
+        });
+        await order.save();
+
+        // create new order canceled publisher, pass in this listener's NATS client
+        // publish function and reference to ticket this order was associated with
+        // to let other services know we have canceled the order b/c it expired
+        new OrderCanceledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+
+        msg.ack();
     }
 }
--- tickets/src/events/publishers/ticket-updated-publisher.ts
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -1,12 +1,6 @@
 import {Publisher, Subjects, TicketUpdatedEvent} from "@mikeytickets/common";
-import { natsWrapper } from "../../nats-wrapper";
 
 
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
     readonly subject = Subjects.TicketUpdated;
-    
 }
-
-new TicketUpdatedPublisher(natsWrapper.client).publish({
-
-});
